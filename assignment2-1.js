e3a14108b04d2c738d91d46919973e7d6c6445b5
Skillasaurus <nickfoulksjsu@gmail>
2016-03-13 18:43:41 -0700

    hey everyone first commit

---
+++ assignment2-1.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+fs.readFile('/etc/hosts', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
+
+fs.readFileSync('/etc/hosts','utf8'){
+
+	
+}
+function listPrimes( nPrimes ) {
+    var primes = [];
+    for( var n = 2;  nPrimes > 0;  n++ ) {
+        if( isPrime(n) ) {
+            primes.push(n);
+            --nPrimes;
+        }
+    }
+    return primes;
+}
+
+function isPrime( n ) {
+    var max = Math.sqrt(n);
+    for( var i = 2;  i <= max;  i++ ) {
+        if( n % i === 0 )
+            return false;
+    }
+    return true;
+}
+fs.writeFileSync( 'test.txt', listPrimes(100) );
